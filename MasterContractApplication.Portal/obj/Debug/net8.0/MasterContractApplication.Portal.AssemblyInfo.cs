94fab32bbe7d58b1b7bb30aad79f57e1ebccc8c9
Andile <AnNgubane@clientele.co.za>
2025-08-06 12:57:36 +0200

    back gound sidebar color

--- MasterContractApplication.Portal/obj/Debug/net8.0/MasterContractApplication.Portal.AssemblyInfo.cs
+++ MasterContractApplication.Portal/obj/Debug/net8.0/MasterContractApplication.Portal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasterContractApplication.Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f989c230259f62f7a7d269403fb48b9f286a6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc77ac33fcac12f1e7235bc4ef0da29268ec5b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasterContractApplication.Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasterContractApplication.Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
