d6f989c230259f62f7a7d269403fb48b9f286a6b
Andile <AnNgubane@clientele.co.za>
2025-08-06 09:21:32 +0200

    master contract dashbord

--- MasterContractApplication.Portal/Program.cs
+++ MasterContractApplication.Portal/Program.cs
@@ -2,19 +2,16 @@ using MasterContractApplication.Portal.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+                .AddInteractiveServerComponents();
 
 builder.Services.AddBlazorBootstrap();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -24,6 +21,6 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+   .AddInteractiveServerRenderMode();
 
 app.Run();
--- MasterContractApplication.Portal/obj/Debug/net8.0/MasterContractApplication.Portal.AssemblyInfo.cs
+++ MasterContractApplication.Portal/obj/Debug/net8.0/MasterContractApplication.Portal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasterContractApplication.Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51f848a80d76d56728e2a61ff048ad73311a6f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551140668387670d142f5ae7a6e79fa1b71b2c04")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasterContractApplication.Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasterContractApplication.Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
